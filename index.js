7dd21dbbce7d47ea9b886df0e11db40b261d85f5
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2020-11-08 13:54:35 +0100

    auto save

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class CoreXStream{
 
     Start(){
         console.log("Application started")
+        this._fs = require("fs");
         const express = require("express");
         const app = express();
 
@@ -23,11 +24,10 @@ class CoreXStream{
     }
 
     VideoStream(VideoFolder, TagName, req, res ){
-        const fs = require("fs");
         const filePath = VideoFolder + "/" + req.query[TagName]
 
         if (req.query[TagName].match(/^[a-z0-9-_ ]+\.(pm4|mov)$/i)){
-            if(fs.existsSync(filePath)){
+            if(this._fs.existsSync(filePath)){
                 const options = {};
                 let start;
                 let end;
@@ -52,66 +52,101 @@ class CoreXStream{
                 }
             
                 res.setHeader("content-type", "video/mp4");
-            
-                fs.stat(filePath, (err, stat) => {
-                    if (err) {
-                        console.error(`File stat error for ${filePath}.`);
-                        console.error(err);
-                        res.sendStatus(500);
-                        return;
+
+                var stats = this._fs.statSync(filePath)
+                var contentLength = stats["size"]
+                if (req.method === "HEAD") {
+                    res.statusCode = 200;
+                    res.setHeader("accept-ranges", "bytes");
+                    res.setHeader("content-length", contentLength);
+                    res.end();
+                }else {       
+                    let retrievedLength;
+                    if (start !== undefined && end !== undefined) {
+                        retrievedLength = (end+1) - start;
+                    }else if (start !== undefined) {
+                        retrievedLength = contentLength - start;
+                    }else if (end !== undefined) {
+                        retrievedLength = (end+1);
+                    }else {
+                        retrievedLength = contentLength;
                     }
-            
-                    let contentLength = stat.size;
-            
-                    // Listing 4.
-                    if (req.method === "HEAD") {
-                        res.statusCode = 200;
+                    res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
+                    res.setHeader("content-length", retrievedLength);
+                    if (range !== undefined) {  
+                        res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
                         res.setHeader("accept-ranges", "bytes");
-                        res.setHeader("content-length", contentLength);
-                        res.end();
-                    }else {       
-                        // Listing 5.
-                        let retrievedLength;
-                        if (start !== undefined && end !== undefined) {
-                            retrievedLength = (end+1) - start;
-                        }else if (start !== undefined) {
-                            retrievedLength = contentLength - start;
-                        }else if (end !== undefined) {
-                            retrievedLength = (end+1);
-                        }else {
-                            retrievedLength = contentLength;
-                        }
-            
-                        // Listing 6.
-                        res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
-            
-                        res.setHeader("content-length", retrievedLength);
-            
-                        if (range !== undefined) {  
-                            res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
-                            res.setHeader("accept-ranges", "bytes");
-                        }
-            
-                        // Listing 7.
-                        const fileStream = fs.createReadStream(filePath, options);
-                        fileStream.on("error", error => {
-                            console.log(`Error reading file ${filePath}.`);
-                            console.log(error);
-                            res.sendStatus(500);
-                        });
-            
-            
-                        fileStream.pipe(res);
                     }
-                });
-                
+                    const fileStream = this._fs.createReadStream(filePath, options);
+                    fileStream.on("error", error => {
+                        console.log(`Error reading file ${filePath}.`);
+                        console.log(error);
+                        res.sendStatus(500);
+                    });
+                    fileStream.pipe(res);
+                }
+
+            
+                // this._fs.stat(filePath, (err, stat) => {
+                //     if (err) {
+                //         console.error(`File stat error for ${filePath}.`);
+                //         console.error(err);
+                //         res.sendStatus(500);
+                //         return;
+                //     }
+            
+                //     let contentLength = stat.size;
+            
+                //     // Listing 4.
+                //     if (req.method === "HEAD") {
+                //         res.statusCode = 200;
+                //         res.setHeader("accept-ranges", "bytes");
+                //         res.setHeader("content-length", contentLength);
+                //         res.end();
+                //     }else {       
+                //         // Listing 5.
+                //         let retrievedLength;
+                //         if (start !== undefined && end !== undefined) {
+                //             retrievedLength = (end+1) - start;
+                //         }else if (start !== undefined) {
+                //             retrievedLength = contentLength - start;
+                //         }else if (end !== undefined) {
+                //             retrievedLength = (end+1);
+                //         }else {
+                //             retrievedLength = contentLength;
+                //         }
+            
+                //         // Listing 6.
+                //         res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
+            
+                //         res.setHeader("content-length", retrievedLength);
+            
+                //         if (range !== undefined) {  
+                //             res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
+                //             res.setHeader("accept-ranges", "bytes");
+                //         }
+            
+                //         // Listing 7.
+                //         const fileStream = this._fs.createReadStream(filePath, options);
+                //         fileStream.on("error", error => {
+                //             console.log(`Error reading file ${filePath}.`);
+                //             console.log(error);
+                //             res.sendStatus(500);
+                //         });
+            
+            
+                //         fileStream.pipe(res);
+                //     }
+                //     const Ticend = Date.now()
+                //     console.log(Ticend - Ticstart)
+                // });
+
             }else {
                 res.status(404).send(`File not foud: ${filePath}`)
             }
         } else {
             res.status(404).send(`File name not match with reg expression: ${req.query[TagName]}`)
         }
-        
     }
 } 
 
